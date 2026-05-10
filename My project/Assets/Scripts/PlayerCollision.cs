5efeed095d82d2703572d2ed0d723952bfbc459f
danielpolland <S00249853@atu.ie>
2026-05-10 13:23:03 +0100

    Fixed issue with player

---
+++ My project/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private PlayerMovement pm;
+
+    private void Awake()
+    {
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Jump")
+        {
+            pm.JumpCount++;
+        }
+
+        if (other.gameObject.tag == "JumpPermanent")
+        {
+            Destroy(other.gameObject);
+            pm.BaseJumpCount++;
+        }
+
+        if (other.gameObject.tag == "Destroy")
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Victory")
+        {
+            Destroy(other.gameObject);
+            pm.BaseJumpCount = 10000;
+        }
+    }
+}
