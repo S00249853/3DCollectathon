115b4489e22f78a20cee5d04fb20c433fdaa25c1
danielpolland <S00249853@atu.ie>
2026-05-18 14:18:02 +0100

    Fixed issues with jumping. added prototype for wall jumping and side flipping. some issues need to be fixed with both

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -16,10 +16,11 @@ public class PlayerMovement : MonoBehaviour
 
     public Vector3 Velocity { get { return characterController.velocity; } }
 
-    private Vector2 movementInput;
+    public Vector2 movementInput;
+    private Vector2 previousMovement;
     private Vector3 movementVelocity;
     private Vector3 movement;
-    private Vector3 lastMovement;
+    private Vector3 appliedMovement;
     private float currentSpeed;
 
     [Header("Settings")]
@@ -43,6 +44,14 @@ public class PlayerMovement : MonoBehaviour
 
     Coroutine CurrentJumpResetRoutine = null;
     bool JumpCountdown;
+    public bool CanSideflip;
+    public bool CanWallJump;
+
+    private Vector3 WallJumpForce;
+
+
+    [SerializeField] private float FlipCd;
+    private float FlipCdTimer;
 
     private void Awake()
     {
@@ -88,16 +97,16 @@ public class PlayerMovement : MonoBehaviour
         else if (isFalling)
         {
             float previousYVelocity = movementVelocity.y;
-            float newYVelocity = movementVelocity.y + (JumpGravities[JumpCount] * fallMultiplier * Time.deltaTime);
-            float nextYVelocity = Mathf.Max((previousYVelocity + newYVelocity) * .5f, -20.0f);
-            movementVelocity.y = nextYVelocity;
+            movementVelocity.y = movementVelocity.y + (JumpGravities[JumpCount] * fallMultiplier * Time.deltaTime);
+            appliedMovement.y = Mathf.Max((previousYVelocity + movementVelocity.y) * .5f, -20.0f);
+       
         }
         else
         {
             float previousYVelocity = movementVelocity.y;
-            float newYVelocity = movementVelocity.y + (JumpGravities[JumpCount] * Time.deltaTime);
-            float nextYVelocity = (previousYVelocity + newYVelocity) * .5f;
-            movementVelocity.y = nextYVelocity;
+            movementVelocity.y = movementVelocity.y + (JumpGravities[JumpCount] * Time.deltaTime);
+            appliedMovement.y = (previousYVelocity + movementVelocity.y) * .5f;
+        
         }
     }
 
@@ -123,7 +132,7 @@ public class PlayerMovement : MonoBehaviour
             }
             IsJumping = true;
             JumpCount++;
-            movementVelocity.y = InitialJumpVelocities[JumpCount] * .5f;
+            movementVelocity.y = InitialJumpVelocities[JumpCount];
         }
         else if (IsJumping && characterController.isGrounded && !IsJumpPressed
             )
@@ -136,22 +145,34 @@ public class PlayerMovement : MonoBehaviour
     public void OnJump(InputAction.CallbackContext obj)
     {
         IsJumpPressed = obj.ReadValueAsButton();
+
+        if (CanWallJump)
+        {
+            CanWallJump = false;
+            movementVelocity = WallJumpForce;
+            Invoke(nameof(ResetWallJump), MaxJumpTime / 2);
+        }
     }
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        //if (!characterController.isGrounded && hit.normal.y < 0.1f)
-        //{
-        //    canWallJump = true;
-        //    WallJumpForce = hit.normal * WalkSpeed;
-        //}
-        //else
-        //{
-        //    canWallJump = false;
-        //}
+        if (!characterController.isGrounded && hit.normal.y < 0.1f)
+        {
+            CanWallJump = true;
+            WallJumpForce = hit.normal * WalkSpeed + transform.up * InitialJumpVelocities[1];
+        }
+        else
+        {
+            CanWallJump = false;
+        }
     }
 
- 
+    private void ResetWallJump()
+    {
+        movementVelocity = Vector3.zero;
+    }
+
+
 
     public void OnDash(InputAction.CallbackContext obj)
     {
@@ -206,11 +227,36 @@ public class PlayerMovement : MonoBehaviour
         {
             DashCdTimer -= Time.deltaTime;
         }
-      //  transform.Rotate(Vector3.up * movement.x);
+
+        if (FlipCdTimer > 0)
+        {
+            FlipCdTimer -= Time.deltaTime;
+        }
+        //  transform.Rotate(Vector3.up * movement.x);
+
+        previousMovement = movementInput;
+
+        if (previousMovement == -movementInput)
+        {
+            if (FlipCdTimer > 0)
+            {
+                return;
+            }
+            else
+            {
+                FlipCdTimer = FlipCd;
+                Debug.Log("Can Sideflip test confirmed");
+                JumpCount = 2;
+            }
+            
+        }
 
         movement = transform.right * movementInput.x + transform.forward * movementInput.y;
 
-        characterController.Move(((movement * currentSpeed) + movementVelocity) * Time.deltaTime);
+        appliedMovement.x = movementVelocity.x;
+        appliedMovement.z = movementVelocity.z;
+
+        characterController.Move(((movement * currentSpeed) + appliedMovement) * Time.deltaTime);
 
         HandleGravity();
         HandleJump();
