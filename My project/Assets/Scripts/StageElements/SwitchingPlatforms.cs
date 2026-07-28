e20ab308350a8c527da3766722bf8057da1bfb28
danielpolland <S00249853@atu.ie>
2026-07-29 00:30:22 +0100

    Minor level clean up. Started working on level underneath the hub area. Added switching platforms, may need to be changed a bit.

---
+++ My project/Assets/Scripts/StageElements/SwitchingPlatforms.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class SwitchingPlatforms : MonoBehaviour
+{
+    [SerializeField] private GameObject[] RightPlatforms;
+    [SerializeField] private GameObject[] LeftPlatforms;
+    [SerializeField] private float PlatformsCd;
+    private float PlatformsCdTimer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        foreach (GameObject go in LeftPlatforms)
+        {
+            Renderer renderer = go.GetComponent<Renderer>();
+            BoxCollider collider = go.GetComponent<BoxCollider>();
+            renderer.enabled = false;
+            collider.enabled = false;
+            PlatformsCdTimer = PlatformsCd;
+        }
+    }
+    private void Update()
+    {
+      if (PlatformsCdTimer > 0) 
+        {
+            PlatformsCdTimer -= Time.deltaTime;
+        } 
+      
+      else
+        {
+            foreach (GameObject go in RightPlatforms)
+            {
+                Renderer renderer = go.GetComponent<Renderer>();
+                BoxCollider collider = go.GetComponent<BoxCollider>();
+                renderer.enabled = !renderer.enabled;
+                collider.enabled = !collider.enabled;
+            }
+
+            foreach (GameObject go in LeftPlatforms)
+            {
+                Renderer renderer = go.GetComponent<Renderer>();
+                BoxCollider collider = go.GetComponent<BoxCollider>();
+                renderer.enabled = !renderer.enabled;
+                collider.enabled = !collider.enabled;
+            }
+            PlatformsCdTimer = PlatformsCd;
+        }
+    }
+}
